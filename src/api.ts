bf87af53675bd460de4d36350dd2d49c8e5a559c
preetamprabhu08 <preetamprabhu08@gmail.com>
2025-05-26 01:13:14 +0530

    base url changed

--- src/api.ts
+++ src/api.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 import { ContactFormData } from './types';
 
 const API_BASE_URL = import.meta.env.PROD 
-  ? 'https://server-ten-ecru.vercel.app/' // Vercel server URL (update this after deployment)
+  ? 'https://server-ten-ecru.vercel.app/api/contact' // Vercel server URL (update this after deployment)
   : 'http://localhost:5000';
 
 export const sendContactForm = async (formData: ContactFormData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/api/contact`, formData);
+    const response = await axios.post(`${API_BASE_URL}`, formData);
     return response.data;
   } catch (error) {
     console.error('Error sending contact form:', error);
